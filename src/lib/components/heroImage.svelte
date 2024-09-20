b262eff53411c6e7da394ede28ac4548591a2adb
brandendupont-mcw <branden.dupont.loyola@gmail.com>
2024-09-20 17:02:13 -0400

    Update heroImage.svelte

--- src/lib/components/heroImage.svelte
+++ src/lib/components/heroImage.svelte
@@ -1,7 +1,7 @@
 
 <script>
 
-const authors = ["Don Stemen","Dave Olson","Patrick Griffin","Amanda Ward", 'Branden DuPont', ]
+const authors = ["Patrick Griffin", 'Branden DuPont',"Don Stemen","Dave Olson","Amanda Ward", ]
 
 const credit ="Kevin Rajaram"
 const image_var ="https://images.unsplash.com/photo-1491832136260-6db1d35247e9?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
