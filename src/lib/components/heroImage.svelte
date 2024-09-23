90dec6e0e58bfe1304815f4e7046d628ef3e1fab
brandendupont-mcw <branden.dupont.loyola@gmail.com>
2024-09-23 13:43:30 -0400

    Update heroImage.svelte

--- src/lib/components/heroImage.svelte
+++ src/lib/components/heroImage.svelte
@@ -28,20 +28,20 @@ import logo from '$lib/assets/images/capitol.jpg';
         
         <div class="sm:mt-10">
         <div class="mx-auto max-w-xl grid sm:text-left">
-          <h2 class="font-mono  text-white  xl:text-5xl  text-4xl font-sans mb-6 ">
+          <h2 class="font-mono  text-white  2xl:text-5xl lg:text-4xl  text-4xl font-sans mb-6 ">
             The First Year of the Pretrial Fairness Act
           </h2>
-          <p class="text-gray-300 sm:text-base  text-sm">
+          <p class="text-gray-300 xl:text-base  text-sm">
  Beginning September 18, 2023, the Pretrial Fairness Act fundamentally altered pretrial practices in Illinois. What can we say about the workings and effects of the new law at the one-year point? 
           </p>
           
           <span class="  font-semibold text-white mt-4"> </span> 
           <ul class="flex flex-row  flex-wrap text-white ">
-            <li class="font-semibold text-white pr-1 sm:text-base  text-xs ">By:</li> 
+            <li class="font-semibold text-white pr-1 xl:text-base  text-xs ">By:</li> 
             {#each authors as author , i}
                <li class="flex flex-wrap max-w-sm" >
 
-                 <dl class=" font-semibold sm:text-base  text-xs">
+                 <dl class=" font-semibold xl:text-base  text-xs">
                    <dt class="sr-only">Name</dt>
                    <dd class="text-white text-mono"> {author} · &nbsp;  </dd>
 
@@ -72,7 +72,7 @@ import logo from '$lib/assets/images/capitol.jpg';
         alt="Image of the Illinois State Capitol by Andrew Adams"
         src={logo }
         aria-label="Image of the Illinois State Capitol by Andrew Adams"
-        class=" w-full   md:h-[450px] lg:h-[450px] xl:h-[620px] 2xl:h-[650px]"
+        class=" w-full   md:h-[450px] lg:h-[450px] xl:h-[500px] 2xl:h-[650px]"
       />
       <div class="absolute bottom-2 right-2  ">
       <div class="text-white text-xs floating-right"><a class="hover:underline hover:text-main" href="https://unsplash.com/@drewjayadams">{credit}</a></div>
